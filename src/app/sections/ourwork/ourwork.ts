97fad545f56c3d320be28e4c4317a3d31f80aa5e
Brett Mitchell <brightpixelstudios@gmail.com>
2026-07-24 00:19:24 -0400

    Update

--- src/app/sections/ourwork/ourwork.ts
+++ src/app/sections/ourwork/ourwork.ts
@@ -11,66 +11,72 @@ import { Work } from '../../models/work';
 })
 export class Ourwork implements OnInit {
   private readonly apiService = inject(ApiService);
+
   work: Work[] = [];
+  toggler: boolean = false;
+  sources: string[] = [];
 
   ngOnInit(): void {
-    this.loadWork(1);
+    this.toggleWebsiteContent();
   }
 
   private loadWork(worktypeid: number): void {
     this.apiService.getWork(worktypeid).subscribe({
       next: (data) => {
         this.work = data;
+        this.setSources();
       },
       error: (err) => console.error('Failed to load work', err),
     });
   }
 
-  // Controller variables
-  toggler = false;
-  sources = [
-    '/images/ourwork/web/large/33_Image.jpg',
-    '/images/ourwork/web/large/30_Image.jpg',
-    '/images/ourwork/web/large/31_Image.jpg',
-    '/images/ourwork/web/large/32_Image.jpg',
-    '/images/ourwork/web/large/33_Image.jpg',
-    '/images/ourwork/web/large/35_Image.jpg',
-    '/images/ourwork/web/large/36_Image.jpg',
-    '/images/ourwork/web/large/37_Image.jpg',
-    '/images/ourwork/web/large/38_Image.jpg',
-    '/images/ourwork/web/large/39_Image.jpg',
-    '/images/ourwork/web/large/30_Image.jpg',
-    '/images/ourwork/web/large/27_Image.jpg',
-    '/images/ourwork/web/large/1_Image.jpg',
-    '/images/ourwork/web/large/2_Image.jpg',
-    '/images/ourwork/web/large/3_Image.jpg',
-    '/images/ourwork/web/large/4_Image.jpg',
-    '/images/ourwork/web/large/5_Image.jpg',
-    '/images/ourwork/web/large/6_Image.jpg',
-    '/images/ourwork/web/large/7_Image.jpg',
-    '/images/ourwork/web/large/8_Image.jpg',
-    '/images/ourwork/web/large/9_Image.jpg',
-    '/images/ourwork/web/large/10_Image.jpg',
-    '/images/ourwork/web/large/11_Image.jpg',
-    '/images/ourwork/web/large/12_Image.jpg',
-    '/images/ourwork/web/large/29_Image.jpg',
-    '/images/ourwork/web/large/14_Image.jpg',
-    '/images/ourwork/web/large/15_Image.jpg',
-    '/images/ourwork/web/large/16_Image.jpg',
-    '/images/ourwork/web/large/17_Image.jpg',
-    '/images/ourwork/web/large/18_Image.jpg',
-    '/images/ourwork/web/large/19_Image.jpg',
-    '/images/ourwork/web/large/20_Image.jpg',
-    '/images/ourwork/web/large/21_Image.jpg',
-    '/images/ourwork/web/large/22_Image.jpg',
-    '/images/ourwork/web/large/23_Image.jpg',
-    '/images/ourwork/web/large/24_Image.jpg',
-    '/images/ourwork/web/large/25_Image.jpg',
-    '/images/ourwork/web/large/26_Image.jpg',
-    '/images/ourwork/web/large/27_Image.jpg',
-    '/images/ourwork/web/large/28_Image.jpg',
-  ];
+  private setSources() {
+    this.toggler = false;
+    this.sources = [
+      '/images/ourwork/web/large/33_Image.jpg',
+      '/images/ourwork/web/large/30_Image.jpg',
+      '/images/ourwork/web/large/31_Image.jpg',
+      '/images/ourwork/web/large/32_Image.jpg',
+      '/images/ourwork/web/large/33_Image.jpg',
+      '/images/ourwork/web/large/35_Image.jpg',
+      '/images/ourwork/web/large/36_Image.jpg',
+      '/images/ourwork/web/large/37_Image.jpg',
+      '/images/ourwork/web/large/38_Image.jpg',
+      '/images/ourwork/web/large/39_Image.jpg',
+      '/images/ourwork/web/large/30_Image.jpg',
+      '/images/ourwork/web/large/27_Image.jpg',
+      '/images/ourwork/web/large/1_Image.jpg',
+      '/images/ourwork/web/large/2_Image.jpg',
+      '/images/ourwork/web/large/3_Image.jpg',
+      '/images/ourwork/web/large/4_Image.jpg',
+      '/images/ourwork/web/large/5_Image.jpg',
+      '/images/ourwork/web/large/6_Image.jpg',
+      '/images/ourwork/web/large/7_Image.jpg',
+      '/images/ourwork/web/large/8_Image.jpg',
+      '/images/ourwork/web/large/9_Image.jpg',
+      '/images/ourwork/web/large/10_Image.jpg',
+      '/images/ourwork/web/large/11_Image.jpg',
+      '/images/ourwork/web/large/12_Image.jpg',
+      '/images/ourwork/web/large/29_Image.jpg',
+      '/images/ourwork/web/large/14_Image.jpg',
+      '/images/ourwork/web/large/15_Image.jpg',
+      '/images/ourwork/web/large/16_Image.jpg',
+      '/images/ourwork/web/large/17_Image.jpg',
+      '/images/ourwork/web/large/18_Image.jpg',
+      '/images/ourwork/web/large/19_Image.jpg',
+      '/images/ourwork/web/large/20_Image.jpg',
+      '/images/ourwork/web/large/21_Image.jpg',
+      '/images/ourwork/web/large/22_Image.jpg',
+      '/images/ourwork/web/large/23_Image.jpg',
+      '/images/ourwork/web/large/24_Image.jpg',
+      '/images/ourwork/web/large/25_Image.jpg',
+      '/images/ourwork/web/large/26_Image.jpg',
+      '/images/ourwork/web/large/27_Image.jpg',
+      '/images/ourwork/web/large/28_Image.jpg',
+    ];
+  }
 
+  // Controller variables
   isVisableWebsiteContent: boolean = true;
   isVisablePrintContent: boolean = false;
   isVisableMobileContent: boolean = false;
@@ -81,6 +87,7 @@ export class Ourwork implements OnInit {
   currentImageMobile: string = this.currentImageDis;
 
   toggleWebsiteContent() {
+    this.loadWork(1);
     this.isVisableWebsiteContent = true;
     this.isVisablePrintContent = false;
     this.isVisableMobileContent = false;
@@ -89,21 +96,23 @@ export class Ourwork implements OnInit {
     this.currentImageMobile = this.currentImageDis;
   }
 
-  togglePrintContent() {
-    this.isVisableWebsiteContent = false;
-    this.isVisablePrintContent = true;
-    this.isVisableMobileContent = false;
-    this.currentImageWebsite = this.currentImageDis;
-    this.currentImagePrint = this.currentImageOver;
-    this.currentImageMobile = this.currentImageDis;
-  }
-
   toggleMobileContent() {
+    this.loadWork(2);
     this.isVisableWebsiteContent = false;
     this.isVisablePrintContent = false;
     this.isVisableMobileContent = true;
     this.currentImageWebsite = this.currentImageDis;
     this.currentImagePrint = this.currentImageDis;
     this.currentImageMobile = this.currentImageOver;
+  }
+
+  togglePrintContent() {
+    this.loadWork(3);
+    this.isVisableWebsiteContent = false;
+    this.isVisablePrintContent = true;
+    this.isVisableMobileContent = false;
+    this.currentImageWebsite = this.currentImageDis;
+    this.currentImagePrint = this.currentImageOver;
+    this.currentImageMobile = this.currentImageDis;
   }
 }
